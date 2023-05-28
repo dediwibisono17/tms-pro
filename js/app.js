17d5909ab96edcdac1166a8dfec4995a84e3b8e7
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-05-28 11:27:48 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ $(".slick-banner").slick(
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
+        // arrows: false,
         autoplaySpeed: 2000,
+        // dots: true
     }
 )
 
